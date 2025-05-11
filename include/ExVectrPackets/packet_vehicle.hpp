1aeaaf1c7de7b82a0cdf7219babf69b7dce0ec03
ChSt15 <chrissteffen98@gmail.com>
2025-05-11 14:15:16 +0200

    Added accuracies to packets

--- include/ExVectrPackets/packet_vehicle.hpp
+++ include/ExVectrPackets/packet_vehicle.hpp
@@ -19,10 +19,14 @@ namespace VCTR
 
             float angularVelocity[3]; // Angular velocity [wx, wy, wz] in rad/s
             float quaternion[4]; // Quaternion [w, x, y, z] Usually tranformation from 
+            float tiltAccuracy; // Tilt accuracy in radians
+            float northAccuracy; // North accuracy in radians
 
-            PacketAttitude(const Math::Vector_F& angVel = 0, const Math::Quat_F& attQuat = Math::Quat_F(1, 0, 0, 0)) {
+            PacketAttitude(const Math::Vector_F& angVel = 0, const Math::Quat_F& attQuat = Math::Quat_F(1, 0, 0, 0), const float tiltAcc = 0, const float northAcc = 0) {
                 for (int i = 0; i < 3; i++) angularVelocity[i] = angVel[i][0];
                 for (int i = 0; i < 4; i++) quaternion[i] = attQuat[i][0];
+                tiltAccuracy = tiltAcc;
+                northAccuracy = northAcc;
             }
 
             Math::Vector_F getAngularVelocity() const {
@@ -38,10 +42,14 @@ namespace VCTR
 
             float position[3]; // Position [x, y, z] in m
             float velocity[3]; // Velocity [vx, vy, vz] in m/s
+            float hAccuracy; // Horizontal accuracy in m
+            float vAccuracy; // Vertical accuracy in m
 
-            PacketPosition(const Math::Vector_F& pos = 0, const Math::Vector_F& vel = 0) {
+            PacketPosition(const Math::Vector_F& pos = 0, const Math::Vector_F& vel = 0, const float hAcc = 0, const float vAcc = 0) {
                 for (int i = 0; i < 3; i++) position[i] = pos[i][0];
                 for (int i = 0; i < 3; i++) velocity[i] = vel[i][0];
+                hAccuracy = hAcc;
+                vAccuracy = vAcc;
             }
 
             Math::Vector_F getPosition() const {
@@ -61,12 +69,19 @@ namespace VCTR
             float velocity[3]; // Velocity [vx, vy, vz] in m/s
             uint8_t numSats; // Number of satellites used for the fix
 
-            PacketGPS(const Math::Vector_F& vel = 0, const float lat = 0, const float lon = 0, const float alt = 0, const uint8_t numSats = 0) {
+            float positionAccuracy; // Position accuracy in meters
+            float altitudeAccuracy; // Altitude accuracy in meters
+            float velocityAccuracy; // Velocity accuracy in m/s
+
+            PacketGPS(const Math::Vector_F& vel = 0, const float lat = 0, const float lon = 0, const float alt = 0, const uint8_t numSats = 0, const float posAcc = 0, const float altAcc = 0, const float velAcc = 0) {
                 for (int i = 0; i < 3; i++) velocity[i] = vel[i][0];
                 latitude = lat * 1e7; // Convert to integer representation
                 longitude = lon * 1e7; // Convert to integer representation
                 altitude = alt;
                 this->numSats = numSats;
+                positionAccuracy = posAcc;
+                altitudeAccuracy = altAcc;
+                velocityAccuracy = velAcc;
             }
 
             double getLatitude() const {
