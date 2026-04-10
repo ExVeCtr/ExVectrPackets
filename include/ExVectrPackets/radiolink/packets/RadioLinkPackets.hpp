074acefc40c57d979615a95a0ffb4274305569a8
chst15 <chrissteffen98@gmail.com>
2026-04-10 15:25:34 +0200

    Added class for filtering out false packets from radiolink

--- include/ExVectrPackets/radiolink/packets/RadioLinkPackets.hpp
+++ include/ExVectrPackets/radiolink/packets/RadioLinkPackets.hpp
@@ -29,4 +29,77 @@ using RadioLinkPacket_A4D4 = RadioLinkPacket<4, 4, RadioLinkTypes::A4D4>;
 using RadioLinkPacket_A6D6 = RadioLinkPacket<6, 6, RadioLinkTypes::A6D6>;
 
 } // namespace VCTR::packets::radiolink
+
+namespace VCTR::packets::radiolink {
+
+/**
+ * @brief The RadioLinkPacketFilter class is responsible for filtering out false
+ * values.
+ */
+template <size_t Ax, size_t Dx, RadioLinkTypes LinkType>
+class RadioLinkPacketFilter {
+private:
+  int16_t analogRejectionThreshold = 200; // Values from -1000 to 1000
+  size_t analogmaxRejectionCount = 1;     // Number of consecutive rejections
+
+  size_t analogValueRejectedClount[Ax]; // How often a channel has been rejected
+
+  int64_t lastUpdatedTimestamp = 0;
+
+  RadioLinkPacket<Ax, Dx, LinkType> filteredLinkPacket;
+
+public:
+  RadioLinkPacketFilter() {
+    for (size_t i = 0; i < Ax; i++) {
+      analogValueRejectedClount[i] = 0;
+      filteredLinkPacket.analogChannels[i] = 0;
+    }
+    for (size_t i = 0; i < Dx; i++) {
+      filteredLinkPacket.digitalChannels[i] = 0;
+    }
+  }
+
+  void setAnalogRejectionThreshold(int16_t threshold) {
+    analogRejectionThreshold = threshold;
+  }
+
+  void setAnalogMaxRejectionCount(size_t count) {
+    analogmaxRejectionCount = count;
+  }
+
+  void setFilteredPacket(const RadioLinkPacket<Ax, Dx, LinkType> &packet) {
+    filteredLinkPacket = packet;
+  }
+
+  const RadioLinkPacket<Ax, Dx, LinkType> &getFilteredPacket() const {
+    return filteredLinkPacket;
+  }
+
+  void updateFromPacket(const RadioLinkPacket<Ax, Dx, LinkType> &packet,
+                        int64_t timeReceived) {
+
+    for (size_t i = 0; i < Ax; i++) {
+      auto value = packet.analogChannels[i];
+      auto lastValue = filteredLinkPacket.analogChannels[i];
+      auto delta = value - lastValue;
+
+      if (std::abs(delta) > analogRejectionThreshold) {
+        analogValueRejectedClount[i]++;
+        if (analogValueRejectedClount[i] > analogmaxRejectionCount) {
+          filteredLinkPacket.analogChannels[i] = value;
+          analogValueRejectedClount[i] = 0;
+        }
+      } else {
+        filteredLinkPacket.analogChannels[i] = value;
+        analogValueRejectedClount[i] = 0;
+      }
+    }
+    for (size_t i = 0; i < Dx; i++) {
+      filteredLinkPacket.digitalChannels[i] = packet.digitalChannels[i];
+    }
+  }
+};
+
+} // namespace VCTR::packets::radiolink
+
 #endif // EXVECTR_RADIOLINK_PACKETS_HPP
